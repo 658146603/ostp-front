289e5714fe210da584a1aa0faa224f1421e3ba9f
h1542462994 <t1542462994@outlook.com>
2020-12-22 09:49:01 +0800

    继续优化前端代码
    1、修改了模板
    2、能够区分四类用户
    3、开始着手编写四种用户对应的不同菜单

--- js/common.js
+++ js/common.js
@@ -3,8 +3,52 @@ class common {
     static book_list = 'book_list'
     static publish_book = 'publish_book'
     static publish_request = 'publish_request'
-    static publish_book2 = 'publish_book2'
+    static book_market = 'book_market'
     static template = 'template'
+    static su = 'su'
+    static su_display = '超级管理员'
+    static admin = 'admin'
+    static admin_display = '学院管理员'
+    static teacher = 'teacher'
+    static teacher_display = '教师'
+    static student = 'student'
+    static student_display = '学生'
+    static defaultAppState() {
+        return {
+            user: null,
+            type: {
+                'role': common.student,
+                'role_display': common.student_display
+            },
+            status: false,
+            tip: '正在加载中...'
+        }
+    }
+    static getUserType(user) {
+        if (user.su === 1) {
+            return {
+                'role': common.su,
+                'role_display': common.su_display
+            }
+        } else if(user.su === 0) {
+            return {
+                'role': common.admin,
+                'role_display': common.admin_display
+            }
+        } else {
+            if (user.college !== undefined) {
+                return {
+                    'role': common.teacher,
+                    'role_display': common.teacher_display
+                }
+            } else {
+                return {
+                    'role': common.student,
+                    'role_display': common.student_display
+                }
+            }
+        }
+    }
     // 获取当前App的状态，包括用户信息等
     static getAppState(callback) {
         request(URL.getLoginStatus, '', (res) => {
@@ -12,12 +56,17 @@ class common {
             if(res.code === 200) {
                 callback({
                     user: res.data,
+                    type: common.getUserType(res.data),
                     status: true,
                     tip: ''
                 })
             } else {
                 callback({
                     user: null,
+                    type: {
+                      'role': common.student,
+                      'role_display': common.student_display
+                    },
                     status: false,
                     tip: res.message
                 })
